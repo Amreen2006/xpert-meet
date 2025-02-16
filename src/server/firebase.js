b82ab551cb0c09449e7af6c301e65f187792d5e2
Amreen Fathimaa <famreen2006@gmail.com>
2025-02-16 10:47:12 +0530

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,10 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyAcvzFXEUPBpUe69uERDTpPh_zXGglmGwI",
-  databaseURL: "https://vivid-alchemy-399203-default-rtdb.firebaseio.com/",
-};
+
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase;
