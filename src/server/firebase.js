79aba5ad7849419ee748e4be6b716214832074c0
Amreen Fathimaa <famreen2006@gmail.com>
2025-02-16 10:47:19 +0530

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -7,9 +7,6 @@ firebase.initializeApp(firebaseConfig);
 export const db = firebase;
 var firepadRef = firebase.database().ref();
 
-const urlparams = new URLSearchParams(window.location.search);
-const roomId = urlparams.get("id");
-
 if (roomId) {
   firepadRef = firepadRef.child(roomId);
 } else {
